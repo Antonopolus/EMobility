84157f30a7d35bca2c7382ac8e0a5ae146c6e861
Antonopolus <schindlbeck@gmx.de>
2021-08-28 13:47:30 +0200

    added web application project

---
+++ EMobility.WebApplication/Controllers/ChargingPointController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMobility.WebApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChargingPointController : ControllerBase
+    {
+        readonly List<ChargingPointModel> ChargingPoints = new();
+
+        private readonly ILogger<ChargingPointController> _logger;
+
+        public ChargingPointController(ILogger<ChargingPointController> logger)
+        {
+            _logger = logger;
+            ChargingPoints = new();
+            ChargingPoints.Add(new(-4, "TG Stellplatz 4", "http://172.16.0.146/rest/", "1384202.00082"));     // Besucherparkplatz TG 4
+            ChargingPoints.Add(new(-5, "TG Stellplatz 5", "http://172.16.0.147/rest/", "1384202.00080"));     // Mayer Thomas
+            ChargingPoints.Add(new(1, "Stellplatz 1", "http://172.16.0.148:81/rest/", "140812422.00057"));    // Besucherparkplatz 1
+            ChargingPoints.Add(new(2, "Stellplatz 2", "http://172.16.0.148:82/rest/", "140812422.00057#2"));  // Besucherparkplatz 2  / slave
+            ChargingPoints.Add(new(3, "Stellplatz 3", "http://172.16.0.149:81/rest/", "140612412.00041"));    // Besucherparkplatz 3
+            ChargingPoints.Add(new(4, "Stellplatz 4", "http://172.16.0.149:82/rest/", "140612412.00041#2"));  // Besucherparkplatz 4  / slave
+        }
+
+        [HttpGet]
+        public IEnumerable<ChargingPointModel> Get()
+        {
+            return ChargingPoints.ToArray();
+        }
+    }
+}
---
+++ EMobilityShared/ChargingPointModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMobility
+{
+    public record ChargingPointModel(int Id, string Name, string RestUrl, string ChargePointId)
+    {
+    }
+}
