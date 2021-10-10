3f945dd80831d8bb395030b97551b801a1da13ce
Antonopolus <82173998+Antonopolus@users.noreply.github.com>
2021-10-10 15:00:42 +0200

    mvc project

---
+++ EMobilitMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EMobilitMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
